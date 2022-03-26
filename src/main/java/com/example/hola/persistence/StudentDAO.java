5fd40f917b9c6c3f634fe91408d9519f41198dae
lauri <laura.granados05@uptc.edu.co>
2022-03-26 17:12:00 -0500

    yy

--- src/main/java/com/example/hola/persistence/StudentDAO.java
+++ src/main/java/com/example/hola/persistence/StudentDAO.java
@@ -1,4 +1,13 @@
 package com.example.hola.persistence;
 
-public interface StudentDAO {
+import com.example.hola.logic.Student;
+
+import java.util.List;
+
+public interface StudentDAO extends java.lang.AutoCloseable{
+    void addStudent(Student student);
+    void updateStudent(Student student);
+    void delete( String code);
+    Student findByCode(String code);
+    List<Student> getAllStudents();
 }
